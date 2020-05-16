f156df83b7da0f604d9471d2366a7fa216ba15fc
Okamoto Yuusuke <yuusuke.o.work.1018@gmail.com>
2020-05-16 13:14:57 +0900

    Change Version Tailwindcss

--- tailwind-react/src/App.js
+++ tailwind-react/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div class="max-w-sm rounded overflow-hidden shadow-lg">
       <img src="https://source.unsplash.com/random" alt="" className="w-full" />
