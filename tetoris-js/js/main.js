39a3a0c858bcba2660a17f72d5dc9d6a4dbdaa85
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-06-02 23:47:04 +0900

    Add Functions

--- tetoris-js/js/main.js
+++ tetoris-js/js/main.js
@@ -2,6 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   const width = 10;
   const height = 20;
+  let currentPosition = 4;
+
+  let squares = Array.from(grid);
+
+  function control(e) {
+    if (e.KeyCode === 39) {
+      moveRight();
+    } else if (e.KeyCode === 38) {
+      rotate();
+    } else if (e.KeyCode === 37) {
+      moveLeft();
+    } else if (e.KeyCode === 40) {
+      moveLeft();
+    }
+  }
+
+  document.addEventListener("keyup", control);
 
   const ltetoris = [
     [1, width + 1, width * 2 + 1, 2],
@@ -44,5 +61,69 @@ document.addEventListener("DOMContentLoaded", () => {
   let currentRotation = 0;
   let current = alltetoris[random][currentRotation];
 
-  let currentPosition;
+  //draw shape
+  function draw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.add("block");
+    });
+  }
+
+  //undraw shape
+  function undraw() {
+    current.forEach((index) => {
+      squares[currentPosition + index].classList.remove("block");
+    });
+  }
+
+  //move and down
+  function moveDown() {
+    undraw();
+    currentPosition = currentPosition += width;
+    draw();
+    freeze();
+  }
+
+  function moveRight() {
+    undraw();
+    const isRightEdge = current.some((index) => {
+      (currentPosition + index) % width === width - 1;
+    });
+    if (!isRightEdge) currentPosition += 1;
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("block2")
+      )
+    ) {
+      currentPosition -= 1;
+    }
+    draw();
+  }
+
+  function moveLeft() {
+    undraw();
+    const isLeftEdge = current.some((index) => {
+      (currentPosition + index) % width === 0;
+    });
+    if (!isLeftEdge) currentPosition -= 1;
+    if (
+      current.some((index) =>
+        squares[currentPosition + index].classList.contains("block2")
+      )
+    ) {
+      currentPosition += 1;
+    }
+    draw();
+  }
+
+  function rotate() {
+    undraw();
+    currentRotation++;
+    if (currentRotation === current.length) {
+      currentRotation = 0;
+    }
+    current = theTetrominoes[random][currentRotation];
+    draw();
+  }
+
+  draw();
 });
