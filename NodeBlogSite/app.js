18cc62ed583a4dbcad7bc642313f19a79bfaa82f
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-06-23 12:40:56 +0900

    Finish Lesson #7

--- NodeBlogSite/app.js
+++ NodeBlogSite/app.js
@@ -1,20 +1,27 @@
 const express = require("express");
 const app = express();
 
+app.set("view engine", "ejs");
+
 app.listen(3000);
 
 app.get("/", (req, res) => {
-  res.sendFile("./views/index.html", { root: __dirname });
-});
-
-app.use((req, res) => {
-  res.status(404).sendFile(__dirname + "/views/404.html");
+  const blogs = [
+    { title: "Hello Node", date: "2020/06/23" },
+    { title: "Hello Ejs", date: "2020/06/23" },
+    { title: "Hello VS code", date: "2020/06/23" },
+  ];
+  res.render("index", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile(__dirname + "/views/about.html");
+  res.render("about", { title: "About" });
 });
 
-app.get("/about-us", (req, res) => {
-  res.sendFile(__dirname + "/views/about.html");
+app.get("/create/blog", (req, res) => {
+  res.render("create", { title: "Post Blog" });
+});
+
+app.use((req, res) => {
+  res.status(404).render("404", { title: "404" });
 });
