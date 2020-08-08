c959a4876dae4331d928967dc1c0c68703068db4
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-08-08 16:26:02 +0900

    Completed Memory Game

--- memory-game/main.js
+++ memory-game/main.js
@@ -1,7 +1,54 @@
 const cards = document.querySelectorAll(".memory-card");
 
-function flipCard() {
-  this.classList.toggle("flip");
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function resetBoard() {
+  [hasFlippedCard, lockBoard] = [false, false];
+  [firstCard, secondCard] = [null, null];
 }
+
+function flipCard() {
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+    hasFlippedCard = true;
+    firstCard = this;
+    return;
+  }
+
+  secondCard = this;
+  checkForMatch();
+}
+
+function checkForMatch() {
+  let isMatch = firstCard.dataset.info === secondCard.dataset.info;
+  isMatch ? disabledCard() : unflipCards();
+}
+
+function disabledCard() {
+  firstCard.removeEventListener("click", flipCard);
+  secondCard.removeEventListener("click", flipCard);
+  resetBoard();
+}
+
+function unflipCards() {
+  lockBoard = true;
+  setTimeout(() => {
+    firstCard.classList.remove("flip");
+    secondCard.classList.remove("flip");
+    resetBoard();
+  }, 1000);
+}
+
+(function shuffle() {
+  cards.forEach((card) => {
+    let randomPos = Math.floor(Math.random() * 12);
+    card.style.order = randomPos;
+  });
+})();
 
 cards.forEach((card) => card.addEventListener("click", flipCard));
