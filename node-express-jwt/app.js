33a7897d52bc3eba121ca254cba17daed3e5606a
Yuusuke Okamoto <yuusuke.o.work.1018@gmail.com>
2020-08-16 23:03:10 +0900

    Make gitignore

--- node-express-jwt/app.js
+++ node-express-jwt/app.js
@@ -11,7 +11,7 @@ app.set("view engine", "ejs");
 
 // database connection
 const dbURI =
-  "mongodb+srv://shaun:test1234@cluster0.del96.mongodb.net/node-auth";
+  "mongodb+srv://dbUser:3kc8tHB5DA5ZsbqV@cluster0.vmcof.mongodb.net/user";
 mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
